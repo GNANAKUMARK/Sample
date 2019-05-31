d8955c7580e84711088b3cd36f19e0f91f4b715c
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-05-31 13:26:59 +0530

    addedresponse entity

--- src/main/java/com/ing/response/SampleResponse.java
+++ src/main/java/com/ing/response/SampleResponse.java
@@ -2,11 +2,22 @@ package com.ing.response;
 
 import java.io.Serializable;
 
+//@Getter @ Setter @NoArgsConstructor
 public class SampleResponse implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	 private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	} 
 
 }
