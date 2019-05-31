d6ecffc00013bdf7c9ba95fce93dce158852fcb1
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-05-31 12:10:52 +0530

    Added log4j implementation

--- src/main/java/com/ing/controller/SampleController.java
+++ src/main/java/com/ing/controller/SampleController.java
@@ -1,5 +1,7 @@
 package com.ing.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,10 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class SampleController {
-	
+	private static final Logger LOGGER = LogManager.getLogger(SampleController.class);
 	@GetMapping(value = "/hello")
 	public void test() {
+		LOGGER.info("Enter SampleController::test");
+		LOGGER.debug("in side test method");
 		System.out.println("Hello Guys");
+		LOGGER.info("Enter SampleController::test");
 		
 	}
 
